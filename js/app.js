d3ffe12bcf9db17eac0a59db29c9bd90712c7190
Julia Iliuk <julia.iluyk@gmail.com>
2018-06-21 15:36:07 -0700

    cleanup before submitting

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ let cardList = ['fa-diamond', 'fa-diamond',
 				'fa-bicycle', 'fa-bicycle',
 				'fa-bomb', 'fa-bomb'];
 
-
 createBoard();
 startGame();
 
@@ -51,16 +50,16 @@ function resetStars() {
  * Shuffle function from http://stackoverflow.com/a/2450976
  */
 
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+function shuffle(desk) {
+    let currentIndex = desk.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        temporaryValue = desk[currentIndex];
+        desk[currentIndex] = desk[randomIndex];
+        desk[randomIndex] = temporaryValue;
     }
-    return array;
+    return desk;
 };
 
 function generateCardHTML(card) {
